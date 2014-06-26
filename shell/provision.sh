101801966412903685a9c36e8201ee6f4c79b240
Randy Geraads <r.geraads@gmail.com>
2014-06-26 13:57:29 +0200

    updated vagrant provisioning

--- shell/provision.sh
+++ shell/provision.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
-apt-get update  >/dev/null
-apt-get install apache2 php5 libapache2-mod-php5 -y
-sed "s/DocumentRoot \/var\/www/DocumentRoot \/var\/www\/web/g" /etc/apache2/sites-available/default > /etc/apache2/sites-available/default.new
-mv /etc/apache2/sites-available/default.new /etc/apache2/sites-available/default
-service apache2 restart
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y apache2 php5 libapache2-mod-php5
+rm -rf /var/www
+ln -fs /vagrant /var/www
