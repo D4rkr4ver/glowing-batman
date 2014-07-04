c6671e53ee2ba6aa5cd0b96fb732c4fc9e184a8b
Randy Geraads <r.geraads@gmail.com>
2014-07-04 13:23:33 +0200

    added xdebug provisioning

--- shell/provision.sh
+++ shell/provision.sh
@@ -5,7 +5,7 @@ echo "************************"
 echo "Installing Apache & PHP5"
 echo "************************"
 apt-get update
-apt-get install -y apache2 php5 libapache2-mod-php5
+apt-get install -y apache2 php5 libapache2-mod-php5 php5-xdebug
 
 echo ""
 echo "***************************************"
@@ -21,4 +21,15 @@ echo "Enabling .htaccess module"
 echo "*************************"
 a2enmod rewrite
 sed -i '/AllowOverride None/c AllowOverride All' /etc/apache2/sites-available/default
+
+echo ""
+echo "***********************"
+echo "Enabling X-Debug module"
+echo "***********************"
+cat <<EOT >> /etc/php5/conf.d/xdebug.ini
+xdebug.remote_enable = on
+xdebug.remote_connect_back = on
+xdebug.idekey = "vagrant"
+EOT
+
 service apache2 restart
