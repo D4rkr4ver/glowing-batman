bf86b375b1cacdd21d666ec563d7dbfc73e63064
Randy Geraads <r.geraads@gmail.com>
2014-11-02 14:31:58 +0100

    added debug mode

--- web/index.php
+++ web/index.php
@@ -8,6 +8,17 @@
 <body>
 <?php
 
+define('DEBUG', true);
+
+if (DEBUG === true) {
+    ini_set('display_errors', 'On');
+    error_reporting(E_ALL);
+    echo '<div class="debug" style="font-weight: bold; text-align: center; border: dashed #ff0000;">Debug Mode</div>';
+} else {
+    ini_set('display_errors', 'Off');
+    error_reporting(0);
+}
+
 require_once(__DIR__ . '/../Autoload.php');
 require_once(__DIR__ . '/../config.php');
 
